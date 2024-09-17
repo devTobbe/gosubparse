9f603a20fe2d38a50f0f4752aabff296626dee91
devTobbe <32337476+devTobbe@users.noreply.github.com>
2024-09-17 18:07:38 +0200

    feat: upgrade SRT regex to also handle VTT

--- internal/util/regex_patterns.go
+++ internal/util/regex_patterns.go
@@ -4,11 +4,12 @@ import (
 	"regexp"
 )
 
-// TODO: Add more formats later on, like .smi (SAMI)
-
+// TODO: Add more formats later on, like .smi (SAMI) and .sub(MircoDVD)
 // Precompiled regular expressions for different subtitle formats
 var (
-	SRT = regexp.MustCompile(`(?m)^(\d+)\n(\d{2}:\d{2}:\d{2},\d{3})\s+-->\s+(\d{2}:\d{2}:\d{2},\d{3})\n((?:[^\n]+\n?)*)`)
-	ASS = regexp.MustCompile(`(?m)^Dialogue:\s*(?:|\w+\W)(?:Marked=\d+,\s*)?\d+,\s*(\d{2}:\d{2}:\d{2}\.\d{2}),\s*(\d{2}:\d{2}:\d{2}\.\d{2}),[^,]*,[^,]*,\d+,\d+,\d+,[^,]*,\s*(.+)$`)
-	VTT = regexp.MustCompile(`(?m)(?:^(\d+)\n)?(\d{2}:\d{2}:\d{2}\.\d{3})\s*-->\s*(\d{2}:\d{2}:\d{2}\.\d{3})\n((?:[^\n]+\n?)*)\n?(?:\n\n|\n\d+|\s*$)`)
+	// Works for both SRT and VTT
+	SRT           = regexp.MustCompile(`(?m)^(\d+)?\n(\d{2}:\d{2}:\d{2}[.,]\d{3})\s+-->\s+(\d{2}:\d{2}:\d{2}[.,]\d{3})(?:[^\n])*\n((?:[^\n]+\n?)+)`)
+	ASS           = regexp.MustCompile(`(?m)^Dialogue:\s*(?:|\w+\W)(?:Marked=\d+,\s*)?\d+,\s*(\d{2}:\d{2}:\d{2}\.\d{2}),\s*(\d{2}:\d{2}:\d{2}\.\d{2}),[^,]*,[^,]*,\d+,\d+,\d+,[^,]*,\s*(.+)$`)
+	VTT           = regexp.MustCompile(`(?m)^(?:WEBVTT\n)?(?:(\d+)\n)?(\d{2}:\d{2}:\d{2}\.\d{3})\s*-->\s*(\d{2}:\d{2}:\d{2}\.\d{3})\n((?:[^\n]+\n?)*)\n`)
+	TagPatternVTT = regexp.MustCompile(`<[^>]+>`)
 )
